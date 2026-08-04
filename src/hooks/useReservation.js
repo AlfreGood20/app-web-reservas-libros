1f4dfd5de2012404dc45bd6b3dc7280527398647
AlfreGood20 <josealfredolopezdelacruz2@gmail.com>
2026-08-04 16:03:21 -0600

    CUSTOM HOOK PARA HACER LA CONSULTA DE RESERVAS POR FILTRO Y PODER CANCELAR RESERVAS

--- src/hooks/useReservation.js
+++ src/hooks/useReservation.js
@@ -1,9 +1,9 @@
 import React, { useEffect, useState } from 'react'
 import { useAuth } from '../context/AuthContext'
-import { getReservas } from '../api/profileApi';
 import { toast } from 'react-toastify';
+import { getReservas, patchReserva } from '../api/reservationApi';
 
-export function useReservation() {
+export function useReservation(estado) {
 
     const {accessToken} = useAuth();
     const [reservas, setReservas] = useState(null);
@@ -18,13 +18,30 @@ export function useReservation() {
 
         setCargando(true);
 
-        getReservas(accessToken)
+        getReservas(accessToken, estado)
             .then(response => response.json())
             .then(datas => setReservas(datas))
             .catch(() => toast.error('Ocurrio un error con el servidor.'))
             .finally(() => setCargando(false));
 
-    }, [accessToken])
+    }, [accessToken, estado])
 
-    return { cargando, reservas }
+
+    const cancelarReserva = async (reservaId) => {
+
+        setCargando(true);
+
+        const response = await patchReserva(accessToken, reservaId);
+        const body = await response.json();
+
+        if(!response.ok){
+            throw new Error(body.menssaje);
+        }
+
+        setReservas(prev => prev.map(r => r.id === reservaId ? body : r));
+
+        setCargando(false);
+    }
+
+    return { cargando, reservas, cancelarReserva}
 }
