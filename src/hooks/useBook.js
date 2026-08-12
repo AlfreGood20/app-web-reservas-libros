262e855167fda3ccf5068792c0acbc74f84c906c
AlfreGood20 <josealfredolopezdelacruz2@gmail.com>
2026-08-11 18:54:07 -0600

    HOOK para libros

---
+++ src/hooks/useBook.js
@@ -0,0 +1,69 @@
+import { useEffect, useState } from 'react'
+import { getLibroPorId, getLibros } from '../api/bookApi';
+import { toast } from 'react-toastify';
+
+
+export function useBook(
+    {
+        page = 0, 
+        size = 20, 
+        sort = 'id,asc',
+        titulo,
+        isbn,
+        categoriaId,
+        editorialId,
+        idiomaId
+    } = {}) {
+
+    const [datos, setDatos] = useState(null);
+    const [cargando, setCargando] = useState(false);
+
+    useEffect(() => {
+
+        const filtros = {page,size, sort, titulo, isbn, categoriaId, editorialId, idiomaId}
+
+        const params = new URLSearchParams(
+            Object.fromEntries(
+                Object.entries(filtros).filter(([_, valor]) => valor !== undefined && valor !== '')
+            )
+        );
+
+        setCargando(true);
+
+        getLibros(params)
+            .then(response => response.json())
+            .then(data => setDatos(data))
+            .catch(() => toast.error("OCURRIÓ UN ERROR EN EL SERVIDOR."))
+            .finally(() => setCargando(false));
+
+    }, [page, size, sort, titulo, isbn, categoriaId, editorialId, idiomaId]);
+
+
+
+    return { datos, cargando };
+}
+
+
+export function useBookGetById(id) {
+
+    const [cargando, setCargando] = useState(false)
+    const [libro, setLibro] = useState(null)
+
+    useEffect(() => {
+
+        if (id === null || id === undefined) {
+            return
+        }
+
+        setCargando(true)
+
+        getLibroPorId(id)
+            .then(response => response.json())
+            .then(data => setLibro(data))
+            .catch(() => toast.error("OCURRIÓ UN ERROR EN EL SERVIDOR."))
+            .finally(() => setCargando(false))
+
+    }, [id])
+
+    return { libro,cargando}
+}
--- src/hooks/useFormData.js
+++ src/hooks/useFormData.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import { getMunicipios, postRegister } from '../api/formApi'
+import { toast } from 'react-toastify';
 
 export function useFormData() {
 
@@ -8,7 +9,7 @@ export function useFormData() {
     useEffect(() => {
         getMunicipios()
             .then(dato => setMunicipios(dato))
-            .catch(error => console.log(error))
+            .catch(error => toast.error('OCURRIÓ UN ERROR EN EL SERVIDOR.'))
     },[]);
 
     return { municipios }
