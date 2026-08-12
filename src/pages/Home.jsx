385b11e30e00a18993f256f89cc80d4792998b20
AlfreGood20 <josealfredolopezdelacruz2@gmail.com>
2026-08-11 18:52:16 -0600

    CATALOGO DE LIBROS 5 PARTES, MAS SKELETON DE CARGA

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,33 +1,108 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import Book from '../components/Book'
 import Navbar from '../components/Navbar'
+import { useBook } from '../hooks/useBook'
+import CatalogBooks from '../components/CatalogBooks'
+import { data } from 'react-router-dom'
 
 function Home() {
 
-  let libro = {
-    portada_url: "https://cdn.prod.website-files.com/6034d7d1f3e0f52c50b2adee/681b63dd7d9dbb4c4ce5ae76_WJlUnXLgNrZqh3HN_u7WMEnTVs1tV0qKwtUkvXJ2JTk.jpeg",
-    titulo : "Principito",
-    anio:1980,
-    isbn:"77-99-0882"
-  }
+  useEffect(() => {
+    document.title = "ME | Catalogo"
+  },[]);
 
+  const dia = new Date().getDay();
+
+  const dias = [
+        { nombre: 'Domingo', letra: 'D' },
+        { nombre: 'Lunes', letra: 'L' },
+        { nombre: 'Martes', letra: 'M' },
+        { nombre: 'Miércoles', letra: 'X' },
+        { nombre: 'Jueves', letra: 'J' },
+        { nombre: 'Viernes', letra: 'V' },
+        { nombre: 'Sábado', letra: 'S' }
+    ];
+
+  const diaActual = dias[dia];
+
+  /* asc|desc */
+  /* BUSCAR PARA LIBROS MAS RECIENTES */
+  const {datos:recienAgregados, cargando:cargandoRecienAgregados} = useBook({ size: 20 });
+
+  /*  BUSCAR LOS LIBROS MAS RECIENTES POR AÑO */
+  const {datos:recientes, cargando:cargandoRecientes} = useBook({ size: 20, sort: 'anio,desc' });
+
+  /* BUSCAR LIBROS CLASICOS POR AÑO */
+  const {datos: clasicos, cargando:cargandoClasicos} = useBook({ size: 20, sort: 'anio,asc'});
+  
+  /* BUSCAR LIBROS CORTOS */
+  const {datos: paraLeer, cargando:cargandoParaLeer} = useBook({ size: 20, sort: 'numeroPaginas,asc' });
+
+  /* BUSCAR LIBROS QUE COMIENZE CON LA LETRA DEL DIA */
+  const { datos: diaSemana, cargando:cargandoDiaSemana } = useBook({ size: 20, titulo: diaActual.letra});
+
+  const Skeleton = () => {
+    return (
+      <div className='flex flex-col gap-3'>
+          <div class="skeleton h-6 w-60"></div>
+          <div className='flex flex-row gap-5'>
+            <div class="skeleton h-50 w-40"></div>
+            <div class="skeleton h-50 w-40"></div>
+            <div class="skeleton h-50 w-40"></div>
+            <div class="skeleton h-50 w-40"></div>
+            <div class="skeleton h-50 w-40"></div>
+            <div class="skeleton h-50 w-40"></div>
+            <div class="skeleton h-50 w-40"></div>
+            <div class="skeleton h-50 w-40"></div>
+          </div>
+        </div>
+    )
+  }
 
   return (
     <>
-        <Navbar />
+    
+        <div className='flex flex-col p-5 gap-8'>
 
-        <div className='flex flex-col p-5'>
+          {cargandoRecienAgregados? 
+              <Skeleton />
+            :
+              <CatalogBooks titulo = 'Recién agregados'>
+                {recienAgregados?.contenido?.map(libro => <Book libro={libro} key={libro.id}/>)}
+              </CatalogBooks>
+          }
 
-          <h1 className='text-lg font-bold'>Libros más recientes</h1>
-          <div className='flex flex-row overflow-x-scroll gap-5'>
+          {cargandoRecientes ? 
+            <Skeleton />
+          :
+            <CatalogBooks titulo= 'Publicados este año / Recientes'>
+              {recientes?.contenido?.map(libro => <Book libro={libro} key={libro.id}/>)}
+            </CatalogBooks>
+          }
 
-            {Array.from({ length: 10 }).map((_, index) => (
-              <Book key={index} libro={libro}/>
-            ))}
+          {cargandoClasicos ? 
+            <Skeleton />
+          :
+             <CatalogBooks titulo = 'Clasicos / Antiguos'>
+                {clasicos?.contenido?.map(libro => <Book libro={libro} key={libro.id}/>)}
+              </CatalogBooks> 
+          }
 
-          </div>
+          {cargandoParaLeer ?
+            <Skeleton />
+          :
+            <CatalogBooks titulo = 'Para terminar un día'>
+              {paraLeer?.contenido?.map(libro => <Book libro={libro} key={libro.id}/>)}
+            </CatalogBooks> 
+          }
 
-          <h1 className='text-lg font-bold'>Libros más pedidos</h1>
+          {cargandoDiaSemana ?
+            <Skeleton />
+          :
+            <CatalogBooks titulo = {`Con ${diaActual.letra} de ${diaActual.nombre}`}>
+              {diaSemana?.contenido?.map(libro => <Book libro={libro} key={libro.id}/>)}
+            </CatalogBooks> 
+          }
 
         </div>
     </>
