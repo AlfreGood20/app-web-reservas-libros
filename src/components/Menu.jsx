b41ca2c5b26524a5f67b18a757598ff889ec3f33
AlfreGood20 <josealfredolopezdelacruz2@gmail.com>
2026-08-14 18:45:53 -0600

    SALIR SESSION LISTO

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,13 +1,29 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import Profile from '../components/Profile'
+import React from 'react';
+import { Link } from 'react-router-dom';
+import Profile from '../components/Profile';
+import { useLogout } from '../hooks/useLogout';
+import { toast } from 'react-toastify';
 
 function Menu({ perfil }) {
+
+    const {cargando, salirSession} = useLogout();
 
     const abrirPerfil = () => {
         document.getElementById('perfil').showModal();
     };
 
+    const handleLogout = async () => {
+        try {
+            await toast.promise(
+                salirSession(),{
+                    pending: "Saliendo sesión...",
+                    success: "Sesión invalida exitosamente.",
+                    error: "OCURRIO UN ERROR EN EL SERVIDOR."
+                }
+            );
+        } catch (error) {}
+    }
+    
     return (
         <div className='flex flex-row items-center gap-5'>
             <ul className="menu bg-base-200 lg:menu-horizontal rounded-box">
@@ -69,13 +85,13 @@ function Menu({ perfil }) {
                         </a>
                     </li>
                     <li>
-                        <a>
+                        <button onClick={handleLogout}>
                             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
                                 <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 9V5.25A2.25 2.25 0 0 0 13.5 3h-6a2.25 2.25 0 0 0-2.25 2.25v13.5A2.25 2.25 0 0 0 7.5 21h6a2.25 2.25 0 0 0 2.25-2.25V15M12 9l-3 3m0 0 3 3m-3-3h12.75" />
                             </svg>
 
                             Salir sesion
-                        </a>
+                        </button>
                     </li>
                 </ul>
             </div>
