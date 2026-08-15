26bfb5ec055f7cd8f49752baca8210e6a1873a0e
AlfreGood20 <josealfredolopezdelacruz2@gmail.com>
2026-08-14 18:42:14 -0600

    NUEVA RUTA Y MANEJO DE RUTAS QUE NO EXISTAN

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import './App.css'
-import Home from './pages/Home'
-import Register from './pages/Register'
-import Login from './pages/Login'
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
+import './App.css';
+import Home from './pages/Home';
+import Register from './pages/Register';
+import Login from './pages/Login';
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-import Layout from './components/Layout'
-import MyActivities from './pages/MyActivities'
+import Layout from './components/Layout';
+import MyActivities from './pages/MyActivities';
+import DetailsBook from './pages/DetailsBook';
+import SearchPanel from './pages/SearchPanel';
 
 function App() {
   
@@ -20,11 +22,13 @@ function App() {
           <Route element={<Layout />}>
             <Route path='/' element={<Home />}/>
             <Route path='/mis-actividades' element={<MyActivities />} />
+            <Route path='/libros/:libro/:id' element={<DetailsBook />} />
+            <Route path='/libros' element={<SearchPanel />} />
         </Route>
 
           <Route path='/register' element={<Register />} />
           <Route path='/login' element={<Login />} />
-         
+          <Route path='*' element={<Navigate to={'/'} replace />}/>
         </Routes>
 
       </BrowserRouter>
